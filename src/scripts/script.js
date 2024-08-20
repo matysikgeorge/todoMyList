f21b94c251fbfb779a4bd5cea33ae3a7a35eafcc
matysikgeorge <matysikgeorge1995@gmail.com>
2024-08-20 10:14:57 +0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,22 +2,99 @@ const mainWrapperNode = document.querySelector(".main__wrapper");
 const formNode = document.querySelector(".main__form");
 const inputForm = document.querySelector(".main__task");
 const buttonAddTask = document.querySelector(".main__button");
+const mainList = document.querySelector(".main__list");
+// const checkboxButton = mainItem.querySelector(".main__item-checkbox");
+const deleteButtonsNone = document.querySelector(".footer__wrapper");
+// const ButtonDeleteAll = document.querySelector(".footer__button-right");
+// const deleteCompletedButton = document.querySelector(".footer__button-left");
+// const deleteItemButton = mainItem.querySelector(".main__item-button");
 
-const todoList = [];
 
-function addTask(text) {}
+let todoList = [];
 
+// - Функция добавления задач
+function addTask(text) {
+  todoList.push({ task: text, isDone: false, id: Date.now() });
+  deleteButtonsNone.classList.remove("footer__wrapper-delete");
+  console.log(todoList);
+  renderTasks(todoList);
+}
+
+// - Создаем логику для form в которой у нас содержится input и кнопка "Добавить" и куда приходит запись информации.
 formNode.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(inputForm.value);
+  addTask(inputForm.value);
   inputForm.value = "";
-
-  // const [input] = event.target;
-  // addTask(input.value);
 });
 
-// к пн добавить таски в списки!!!
-// добить стили!
+//- Создаем функцию создания задач
+function createTask(arrayTask) {
+  const mainItem = document.createElement("li");
+  mainItem.className = "main__item";
+  mainItem.innerHTML = `
+              <input class="main__item-checkbox" type="checkbox"/>
+              <p class="main__item-text">${arrayTask.task}</p>  
+              <button class="main__item-button">❌</button>
+  `;
 
+  //- Функция провреки нажатия checkbox
 
+  const checkboxButton = mainItem.querySelector(".main__item-checkbox");
+  checkboxButton.addEventListener("click", () => {
+    if (checkboxButton.checked) {
+      arrayTask.isDone = true;
+    } else {
+      arrayTask.isDone = false;
+    }
+    console.log(arrayTask);
+  });
 
+  const deleteItemButton = mainItem.querySelector(".main__item-button");
+  deleteItemButton.addEventListener("click", () =>
+    deleteCrossButton(arrayTask.id)
+  );
+  return mainItem;
+}
+
+// - Функция удаления элементов по крестику
+function deleteCrossButton(id) {
+  todoList = todoList.filter((el) => el.id !== id);
+  renderTasks(todoList);
+}
+
+//- Функция удаления завершенных задач
+function getDeletedCompletedButton() {
+  todoList = todoList.filter((el) => el.isDone !== true);
+  renderTasks(todoList);
+}
+
+//- Создаем функцию отрисовки 
+function renderTasks(newData) {
+  mainList.innerHTML = ""; // очищаем всю форму, но при вводе нового таска мы увидим удаленный task. В самом массиве значение остается и плюсуется к старому массиву.
+  newData.forEach((element) => {
+    const words = createTask(element);
+    mainList.append(words); // куда, что делаем и что добавляем
+  });
+  deleteBtn()
+}
+
+const deleteCompletedButton = document.querySelector(".footer__button-left");
+deleteCompletedButton.addEventListener("click", getDeletedCompletedButton);
+
+const ButtonDeleteAll = document.querySelector(".footer__button-right");
+ButtonDeleteAll.addEventListener("click", () => {
+  mainList.innerHTML = "";
+  todoList = []; // либо 0, либо "";
+  // deleteButtonsNone.classList.add("footer__wrapper-delete");
+  deleteBtn()
+});
+
+// прописать логику для скрытия нижних кнопок, закрытие задач по крестикам и по завершенным.
+// и при создании тасков вовзвращались кнопки
+// класс - list, а класс remove
+
+function deleteBtn() {
+  if (todoList.length === 0) {
+    deleteButtonsNone.classList.add("footer__wrapper-delete");
+  }
+}
---
+++ src/scripts/комментарии.js
@@ -0,0 +1,32 @@
+// const mainWrapperNode = document.querySelector(".main__wrapper");
+// const formNode = document.querySelector(".main__form");
+// const inputForm = document.querySelector(".main__task");
+// const buttonAddTask = document.querySelector(".main__button"); // - Для создания узла - создаем переменную 'const' и задаем информативное имя 'buttonAddTask =' ('кнопкаДобавленияЗадач =') - (не обязательно использовать 'Node', имя может быть любым главное информативным). Далее через оператор присвоения '=' обращаемся к документу 'document' далее с помощью метода '.querySelector(".main__button");' - привязываемся к классу документа который передаем в параметр и с которым будем взаимодействовать.
+// .querySelector() - метод позволяет находить элементы внутри DOM. принимает один аргумент — строку CSS-селектора, и возвращает первый элемент внутри документа, который соответствует этому селектору.
+
+// const todoList = []; // - создаем пустой массив (сначала обязательно создаем переменную 'const' потом имя массива 'todoList =' и сам пустой массив [])
+
+// - Функция добавления задач
+// function addTask(text) {
+//   todoList.push({ task: text });
+//   console.log(todoList);
+// }
+// function addTask(text) { // - создаем функцию 'function' и имя 'addTask(text)' в параметр функции принимает в себя(text), открываем тело функции {}
+// todoList.push({task: text}); // - в теле функции обращаемся к массиву 'todoList' и с помощью метода '.push()' - добавляем объект массива в параметр ( и передаем {ключ 'task:' с значением 'text'});
+// '.push()' - добавляем один/несколько элементов в конец массива (результате вернётся новая длина массива)
+//   console.log(todoList);
+// }
+
+// - Создаем логику для form в которой у нас содержится input и кнопка "Добавить" и куда приходит запись информации.
+// formNode.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(inputForm.value);
+//   inputForm.value = "";
+// });
+
+// formNode.addEventListener("submit", (event) => {   //- обращаемся к узлу 'formNode' с помощью метода .ddEventListener("submit", (event) => {}) - 1-й параметр свойство "submit" - которое позволяет собрать данные при нажатии на кнопку, 2-й параметр - это функция, где 'event' - это событие которое описывает событие, произошедшее на странице -
+// - .addEventListener() - это функция в JS, которая принимает в параметр событие и второй аргумент.
+// event.preventDefault(); // - .preventDefault() в JS используется для отмены действия по умолчанию (в данном случае для отмены автоматического обновления страницы при нажатии на кнопку)
+// console.log(inputForm.value); // - отобразить в консоли значение 'inputForm.value'
+// inputForm.value = ""; // -  значение переменной 'inputForm.value' равное пустому значению
+// });
